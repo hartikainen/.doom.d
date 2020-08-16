926321bdbe5b1881dde5cdf7325cb5948aca6ac6
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-08-16 13:36:19 +0300

    Add functions.el

---
+++ functions.el
@@ -0,0 +1,19 @@
+;;; functions.el -*- lexical-binding: t; -*-
+
+(defun copy-strip-whitespace (&optional beg end)
+  "Save the current region (or line) to the `kill-ring' after stripping extra whitespace and new lines"
+  (interactive
+   (if (region-active-p)
+       (list (region-beginning) (region-end))
+     (list (line-beginning-position) (line-end-position))))
+  (let ((my-text (buffer-substring-no-properties beg end)))
+    (with-temp-buffer
+      (insert my-text)
+      (goto-char 1)
+      (while (looking-at "[ \t\n]")
+        (delete-char 1))
+      (let ((fill-column 9333999))
+        (fill-region (point-min) (point-max)))
+      (set-mark (point-min))
+      (goto-char (point-max))
+      (clipetty-kill-ring-save))))
