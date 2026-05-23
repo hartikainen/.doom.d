13b1f17aeaafc63c2913091e0e62860fdffe0c01
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2026-05-23 19:16:50 -0400

    Enable `(kotlin +lsp +tree-sitter)`

--- init.el
+++ init.el
@@ -144,7 +144,7 @@
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
        (javascript +lsp +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
-       ;;kotlin            ; a better, slicker Java(Script)
+       (kotlin +lsp +tree-sitter)            ; a better, slicker Java(Script)
        (latex +lsp)      ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
